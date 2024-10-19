1348fdd1f7c774494c1e9aaed7b8a3e766e63a5a
cesarfkb <cesarfkb12@gmail.com>
2024-10-19 17:38:25 -0300

    feat: criação de dicionário de erros e exibição correta de erro

--- tela_erro.py
+++ tela_erro.py
@@ -1,10 +1,16 @@
 import tkinter as tk
 from tkinter import messagebox
 
+ERROS = {'NUMERICO': 'Erro: o código digitado não é válido. O código não pode conter letras ou símbolos.',
+         'BINARIO': 'Erro: o código digitado não é válido. O código deve conter apenas 0s e 1s.',
+         'TAMANHO': 'Erro: o código digitado não é válido. O código deve ter um tamanho múltiplo de 32.',
+         'VAZIO': 'Erro: o código digitado não é válido. O código não pode estar vazio.'}
+
 def tela_erro(mensagem: str):
+    mensagem = ERROS.get(mensagem, mensagem)
     root = tk.Tk()
     root.withdraw()
-    messagebox.showerror("Erro", mensagem)
+    messagebox.showerror('Erro', mensagem)
     root.destroy()
     
 if '__main__' == __name__:
