6ac4cbd131814c18ab62aaa8253d8cf9cfaada4c
cesarfkb <cesarfkb12@gmail.com>
2024-10-19 20:55:21 -0300

    feat: criação da tela de detalhes e resultados para a exbição da execução do código

---
+++ tela_detalhes.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+
+def tela_detalhes(comandos: list) -> None:
+    
+    lista_comandos = ''
+    count = 1
+    
+    for comando in comandos:
+        lista_comandos += f'{count} - {comando}\n'
+        count += 1
+    
+    def done():
+        root.destroy()
+        
+    root = tk.Tk()
+    root.title("Detalhes Comandos")
+    
+    commands_label = tk.Label(root, text="Comandos Executados" + "\n" + lista_comandos)
+    commands_label.pack(pady=10)
+    
+    done_button = tk.Button(root, text="Fechar", command=done)
+    done_button.pack(side=tk.LEFT, padx=20, pady=20)
+    
+    root.mainloop()
