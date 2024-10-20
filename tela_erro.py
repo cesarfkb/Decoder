0b75775a5b679078e5ffd7ee38219064ab928da2
cesarfkb <cesarfkb12@gmail.com>
2024-10-19 21:13:27 -0300

    feat: adição de mais mensagens de erro para o dicionário de erros

--- tela_erro.py
+++ tela_erro.py
@@ -4,7 +4,10 @@ from tkinter import messagebox
 ERROS = {'NUMERICO': 'Erro: o código digitado não é válido. O código não pode conter letras ou símbolos.',
          'BINARIO': 'Erro: o código digitado não é válido. O código deve conter apenas 0s e 1s.',
          'TAMANHO': 'Erro: o código digitado não é válido. O código deve ter um tamanho múltiplo de 32.',
-         'VAZIO': 'Erro: o código digitado não é válido. O código não pode estar vazio.'}
+         'VAZIO': 'Erro: o código digitado não é válido. O código não pode estar vazio.',
+         'OPCODE': 'Erro: o código digitado não é válido. O opcode não é reconhecido.',
+         'FUNC': 'Erro: o código digitado não é válido. A função do comando não é reconhecida.',
+         'GENERIC': 'Erro: o código digitado não é válido. O comando não pôde ser executado.'}
 
 def tela_erro(mensagem: str):
     mensagem = ERROS.get(mensagem, mensagem)
